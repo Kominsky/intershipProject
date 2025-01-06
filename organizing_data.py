994074eb0ffab24460bd170509ed9414789758f9
Kominsky <127443744+Kominsky@users.noreply.github.com>
2025-01-06 14:59:26 -0700

    tying to go though the path and print it out

--- organizing_data.py
+++ organizing_data.py
@@ -3,3 +3,9 @@ import shutil
 
 def organinize_files(directory):
     for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            print(filename) #Print the file names to see if the loop is working
+
+
+#testing the function
+organinize_files("/path/to/test/directory")
